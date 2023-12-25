3ab79877fa7ea2ba4e7ba43b8623986e3ec8b239
De1fy <103572164+cegou121@users.noreply.github.com>
2023-12-25 11:55:38 +0200

    Update and rename SnowEffectPlugim to SnowItem.java

---
+++ SnowItem.java
@@ -0,0 +1,12 @@
+package snowitem.snowsitemscore;
+
+import org.bukkit.Material;
+
+public class ItemBuilder {
+    public ItemBuilder(Material diamondSword) {
+
+    }
+
+    public void name(String s) {
+    }
+}
