cebf8cedb026c3b49b78d5ee053e07d9db1aae61
De1fy <103572164+cegou121@users.noreply.github.com>
2024-01-01 14:33:36 +0200

    Update SnowItem.java

--- SnowItem.java
+++ SnowItem.java
@@ -1,12 +1,1 @@
 package snowitem.snowsitemscore;
-
-import org.bukkit.Material;
-
-public class ItemBuilder {
-    public ItemBuilder(Material diamondSword) {
-
-    }
-
-    public void name(String s) {
-    }
-}
